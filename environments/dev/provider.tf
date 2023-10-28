fe393ab9bf3f0e329c203ebc38098c9d3465c671
Pavlo <paulsegfault@gmail.com>
2023-10-28 22:53:26 +0300

    Added base structure

---
+++ environments/dev/provider.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+
+}
+
+terraform {
+  backend "s3" {
+    encrypt                 = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
